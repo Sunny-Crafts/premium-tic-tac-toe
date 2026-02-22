972d45a20c24c06ad34d13c02798adff82e0f819
Sunny <sunny08128@gmail.com>
2026-02-22 11:15:31 +0530

    Fix Express 5 wildcard route for Render deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 // Frontend Routes (SPA support)
-app.get(['/', '/play/*'], (req, res) => {
+app.get(['/', '/play/:path*'], (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
